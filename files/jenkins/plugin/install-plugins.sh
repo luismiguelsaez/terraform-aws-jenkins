bf6992ce2b8a0b97cec49a0b0cfe8bfd17e32b4e
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-08-06 23:17:19 +0200

    Added plugins install script

---
+++ files/jenkins/plugin/install-plugins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+JENKINS_HOME=$1
+shift
+PLUGINS_LIST=$@
+
+doInstall(){
+
+ NAME=$(echo $1 | cut -f1 -d:)
+ VERS=$(echo $1 | cut -f2 -d:)
+
+ if [ ! -d ${JENKINS_HOME}/plugins/${NAME} ]
+ then 
+  curl -sL https://updates.jenkins-ci.org/latest/${NAME}.hpi -o ${JENKINS_HOME}/plugins/${NAME}.hpi
+
+  unzip -qu -d ${JENKINS_HOME}/plugins/${NAME} ${JENKINS_HOME}/plugins/${NAME}.hpi
+
+  if `grep "Plugin-Dependencies:" ${JENKINS_HOME}/plugins/${NAME}/META-INF/MANIFEST.MF >/dev/null 2>&1`
+  then
+    for PD in $(grep "Plugin-Dependencies:" ${JENKINS_HOME}/plugins/${NAME}/META-INF/MANIFEST.MF | sed 's/Plugin-Dependencies://g')
+    do
+      doInstall $PD
+    done
+  fi
+  return 1
+ else
+  return 0
+ fi
+}
+
+RES=0
+ 
+for PLUGIN in ${PLUGINS_LIST}
+do
+  doInstall $PLUGIN
+  RES=$(($RES + $?))
+done
+
+exit $RES
